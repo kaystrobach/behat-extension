61b1d18cb4261b17d7b78da1df4d20cec0c2ec08
Kay Strobach <info@kay-strobach.de>
2015-05-14 17:38:40 +0200

    [TASK] fix ssl tester

---
+++ Classes/KayStrobach/BehatExtension/Context/SslQualysContext/Exceptions/NoSslException.php
@@ -0,0 +1,8 @@
+<?php
+namespace KayStrobach\BehatExtension\Context\SslQualysContext\Exceptions;
+
+use Behat\Mink\Exception\ExpectationException;
+
+class NoSslException extends ExpectationException{
+
+}
--- Classes/KayStrobach/BehatExtension/Context/SslQualysContext/FeatureContext.php
+++ Classes/KayStrobach/BehatExtension/Context/SslQualysContext/FeatureContext.php
@@ -3,6 +3,7 @@
 namespace KayStrobach\BehatExtension\Context\SslQualysContext;
 use Behat\Mink\Exception\ExpectationException;
 use KayStrobach\BehatExtension\Context\AbstractFeatureContext;
+use KayStrobach\BehatExtension\Context\SslQualysContext\Exceptions\NoSslException;
 
 
 class FeatureContext extends AbstractFeatureContext{
@@ -12,7 +13,8 @@ class FeatureContext extends AbstractFeatureContext{
 		'B' => 80,
 		'C' => 60,
 		'D' => 40,
-		'E' => 30
+		'E' => 30,
+		'T' => 0,
 	];
 
 	/**
@@ -34,17 +36,29 @@ class FeatureContext extends AbstractFeatureContext{
 	}
 
 	/**
-	 * @param string $url
+	 * @param string $uri
 	 * @return string
 	 * @throws \Exception
 	 */
-	protected function getCurrentSslVoting($url) {
-		$buffer = file_get_contents('https://www.ssllabs.com/ssltest/analyze.html?d=' . urlencode($this->getCurrentLocation()));
+	protected function getCurrentSslVoting($uri) {
+		$pathSegments = parse_url($uri);
+		if(!array_key_exists('host', $pathSegments)) {
+			throw new \Exception('The Uri' . $uri . ' can´ be parsed cleanly');
+		}
+		$buffer = file_get_contents('https://www.ssllabs.com/ssltest/analyze.html?d=' . urlencode($pathSegments['host']) . '&s=' . gethostbyname($pathSegments['host']));
 		libxml_use_internal_errors(true);
 		$domDocument = new \DOMDocument();
 		$domDocument->loadHTML($buffer);
 		$ratingElement = $domDocument->getElementById('rating');
 		if($ratingElement === NULL) {
+			$warningBox = $domDocument->getElementById('warningBox');
+			if($warningBox !== null) {
+				switch (trim($warningBox->textContent)) {
+					case 'Assessment failed: Unable to connect to server':
+						throw new NoSslException(trim($warningBox->textContent), $this->getSession());
+					break;
+				}
+			}
 			throw new \Exception('No voting available yet');
 		}
 		libxml_use_internal_errors(false);
@@ -60,7 +74,7 @@ class FeatureContext extends AbstractFeatureContext{
 			$stateValue = $this->sslStates[$state];
 		} else {
 			$this->printDebug('the given state is unknown ... use A+, A, B, C, D, E please');
-			$stateValue = 90;
+			$stateValue = 0;
 		}
 		return $stateValue;
 	}
@@ -71,13 +85,38 @@ class FeatureContext extends AbstractFeatureContext{
 	 * @Then /^the Qualys SSL check should be atleast "([^"]*)"$/
 	 */
 	public function theSslCheckShouldBeAtleast($state) {
+		$this->theSslForUriCheckShouldBeAtleast(
+			$state,
+			$this->getCurrentLocation()
+		);
+	}
+
+	/**
+	 * @param $state
+	 * @param $uri
+	 * @throws ExpectationException
+	 * @throws \Exception
+	 *
+	 * @Then /^the domain Qualys SSL check should be atleast "([^"]*)" for uri "([^"]*)"$/
+	 */
+	public function theSslForUriCheckShouldBeAtleast($state, $uri) {
 		$stateAsInteger = $this->mapStateToInteger($state);
 		try {
-			$voting = $this->getCurrentSslVoting('https://www.ssllabs.com/ssltest/analyze.html?d=' . urlencode($this->getCurrentLocation()) . '&clearCache=on');
-		} catch(\Exception $e) {
+			$voting = $this->getCurrentSslVoting($uri);
+		} catch(NoSslException $e) {
+			throw $e;
+		} catch (\Exception $e) {
 			$this->printDebug('Connection to ssllabs.com for initializing the check, may take several minutes ...');
-			sleep(300);
-			$voting = $this->getCurrentSslVoting('https://www.ssllabs.com/ssltest/analyze.html?d=' . urlencode($this->getCurrentLocation()) . '&clearCache=on');
+			$time = time();
+			while(time() - $time <= 300) {
+				echo '|';
+				usleep(500);
+				echo "\r";
+				echo '-';
+				usleep(500);
+				echo "\r";
+			}
+			$voting = $this->getCurrentSslVoting($uri);
 		}
 		$votingAsInteger = $this->mapStateToInteger($voting);
 		if(($stateAsInteger > $votingAsInteger)) {
