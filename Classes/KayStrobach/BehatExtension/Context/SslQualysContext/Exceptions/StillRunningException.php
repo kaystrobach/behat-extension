e551a7576c7a0e96d3b52d0624e107ccc39d4f4a
Kay Strobach <info@kay-strobach.de>
2015-05-27 11:58:26 +0200

    [TASK] fix problem with multiple ip DNS ssl

---
+++ Classes/KayStrobach/BehatExtension/Context/SslQualysContext/Exceptions/StillRunningException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kay
+ * Date: 27.05.15
+ * Time: 11:39
+ */
+
+namespace KayStrobach\BehatExtension\Context\SslQualysContext\Exceptions;
+
+
+use Behat\Mink\Exception\ExpectationException;
+
+class StillRunningException extends ExpectationException{
+
+}
--- Classes/KayStrobach/BehatExtension/Context/SslQualysContext/FeatureContext.php
+++ Classes/KayStrobach/BehatExtension/Context/SslQualysContext/FeatureContext.php
@@ -4,6 +4,7 @@ namespace KayStrobach\BehatExtension\Context\SslQualysContext;
 use Behat\Mink\Exception\ExpectationException;
 use KayStrobach\BehatExtension\Context\AbstractFeatureContext;
 use KayStrobach\BehatExtension\Context\SslQualysContext\Exceptions\NoSslException;
+use KayStrobach\BehatExtension\Context\SslQualysContext\Exceptions\StillRunningException;
 
 
 class FeatureContext extends AbstractFeatureContext{
@@ -40,12 +41,16 @@ class FeatureContext extends AbstractFeatureContext{
 	 * @return string
 	 * @throws \Exception
 	 */
-	protected function getCurrentSslVoting($uri) {
+	protected function getCurrentSslVoting($uri, $ip = NULL) {
 		$pathSegments = parse_url($uri);
-		if(!array_key_exists('host', $pathSegments)) {
-			throw new \Exception('The Uri' . $uri . ' can´ be parsed cleanly');
+		if($ip === NULL) {
+			if(!array_key_exists('host', $pathSegments)) {
+				throw new \Exception('The Uri' . $uri . ' can´ be parsed cleanly');
+			}
+			$ip = gethostbyname($pathSegments['host']);
 		}
-		$buffer = file_get_contents('https://www.ssllabs.com/ssltest/analyze.html?d=' . urlencode($pathSegments['host']) . '&s=' . gethostbyname($pathSegments['host']));
+
+		$buffer = file_get_contents('https://www.ssllabs.com/ssltest/analyze.html?d=' . urlencode($pathSegments['host']) . '&s=' . $ip);
 		libxml_use_internal_errors(true);
 		$domDocument = new \DOMDocument();
 		$domDocument->loadHTML($buffer);
@@ -103,22 +108,36 @@ class FeatureContext extends AbstractFeatureContext{
 	 * @Then /^the domain Qualys SSL check should be atleast "([^"]*)" for uri "([^"]*)"$/
 	 */
 	public function theSslForUriCheckShouldBeAtleast($state, $uri) {
+		$this->theSslForUriCheckShouldBeAtleastForIp($state, $uri);
+	}
+
+	/**
+	 * @param $state
+	 * @param $uri
+	 * @param $ip
+	 * @throws ExpectationException
+	 * @throws \Exception
+	 *
+	 * @Then /^the domain Qualys SSL check should be atleast "([^"]*)" for uri "([^"]*)" and ip "([^"]*)"$/
+	 */
+	public function theSslForUriCheckShouldBeAtleastForIp($state, $uri, $ip = NULL) {
 		$stateAsInteger = $this->mapStateToInteger($state);
 		echo chr(10);
-		$this->prettyPrintDebug('Starting SSL check for ' . $uri);
+		$this->prettyPrintDebug('ssllabs.com: Starting check for ' . $uri);
 
 		for($i = 0; $i < 20; $i++) {
 			try {
-				$voting = $this->getCurrentSslVoting($uri);
+				$voting = $this->getCurrentSslVoting($uri, $ip);
 				continue;
 			} catch(NoSslException $e) {
 				throw $e;
-			} catch (\Exception $e) {
+			} catch (StillRunningException $e) {
 				$this->prettyPrintDebug('ssllabs.com: ' . $e->getMessage());
 				sleep(30);
 			}
 		}
 
+		$this->prettyPrintDebug('ssllabs.com: current voting is ' . $voting);
 		$votingAsInteger = $this->mapStateToInteger($voting);
 		if(($stateAsInteger > $votingAsInteger)) {
 			throw new ExpectationException('SSL State ' . $voting. ' is lower than allowed', $this->getSession());
